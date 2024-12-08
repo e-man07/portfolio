8b83ca7f8ee4c61015188f133ff448673acbdc96
jaydattkaran <karanjaydatt03@gmail.com>
2024-12-08 23:36:24 +0530

    initial commit

---
+++ src/components/homepage/Role.jsx
@@ -0,0 +1,12 @@
+
+export default function Role({forwardedRef}) {
+  return (
+    <section ref={forwardedRef} id="about" className="select-none flex my-20 md:my-[12%] py-10 flex-col items-center justify-center overflow-hidden nav-change" aria-label="">
+      <div className="flex w-full items-center space-x-20">
+        <h1 className="text-heading-1 font-medium text-secondary-400 leading-[1.25em] md:leading-[1.08em]">
+        I craft dynamic digital experiences that inspire and connect with users by combining full-stack development expertise with cutting-edge Web3 innovations.
+        </h1>
+      </div>
+    </section>
+  );
+}
